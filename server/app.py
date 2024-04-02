3b9b462fa2f6c8909667c0d493eb779ec3cec573
Ben Cavins <ben.cavins@flatironschool.com>
2024-04-02 15:01:25 -0400

    do lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,35 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    eq = Earthquake.query.filter(Earthquake.id == id).first()
 
+    if eq is None:
+        return make_response({'message': f'Earthquake {id} not found.'}, 404)
+
+    return make_response(eq.to_dict(), 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_mag(magnitude):
+    # queried the db for Earthquake objects
+    eqs = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(eqs)
+
+    # mapped each Earthquake obj to a dict
+    eq_dicts = []
+    for eq_obj in eqs:
+        eq_dicts.append(eq_obj.to_dict())
+    # eq_dicts = [eq.to_dict() for eq in eqs]  # can use listcomp
+
+    # build our response dict
+    data = {
+        'count': count,
+        'quakes': eq_dicts
+    }
+
+    # returned a response
+    return make_response(data, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
